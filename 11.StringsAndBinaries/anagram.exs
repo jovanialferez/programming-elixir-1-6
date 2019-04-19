fdd6f5970015a1ac27c24b96ceeb3311ac04d942
jovanialferez <vojalf@gmail.com>
2019-04-19 17:28:29 +0800

    Some docs for anagram.

--- 11.StringsAndBinaries/anagram.exs
+++ 11.StringsAndBinaries/anagram.exs
@@ -1,11 +1,18 @@
 defmodule Awesome do
     def anagram?(word1, word2) when word1 == word2, do: true
+    @doc """
+    Simple algo for anagram: sort then compare.
+    """
     def anagram?(word1, word2) do
         sorted1 = word1 |> trimAndSort
         sorted2 = word2 |> trimAndSort
 
         sorted1 == sorted2
     end
+
+    @doc """
+    Trims any spaces (?\s) then sort.
+    """
     defp trimAndSort(word) do
         word 
             |> Enum.filter(&(&1 != ?\s))
